4513b38ce94ac4c777aebcbfa1c14c2b2666bcf3
Michael Heimpold <michael.heimpold@i2se.com>
2018-04-12 09:42:35 +0200

    tarragon: adapt boot stuff for rauc support
    Signed-off-by: Michael Heimpold <michael.heimpold@i2se.com>

--- include/configs/tarragon.h
+++ include/configs/tarragon.h
@@ -1,13 +1,12 @@
 /*
- * Copyright (C) 2017 I2SE GmbH
+ * Copyright (C) 2017-2018 I2SE GmbH
  *
  * Configuration settings for the I2SE Tarragon board.
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
-#ifndef __TARRAGON_CONFIG_H
-#define __TARRAGON_CONFIG_H
-
+#ifndef __CONFIGS_TARRAGON_H__
+#define __CONFIGS_TARRAGON_H__
 
 #include <asm/arch/imx-regs.h>
 #include <linux/sizes.h>
@@ -78,6 +77,39 @@
 	"bootcmd_mfg=run mfgtool_args;bootz ${loadaddr} ${initrd_addr} ${fdt_addr};\0" \
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
+	"update_fw_filename_prefix=emmc.img.\0" \
+	"update_fw_filename_suffix=.gz\0" \
+	"update_fw_parts=0x6\0" \
+	"update_fw_fsize_uncompressed=4000000\0" \
+	"gzwrite_wbuf=100000\0" \
+	"update_emmc_firmware=" \
+		"setexpr i ${update_fw_parts}; setexpr error 0; " \
+		"while itest ${i} -gt 0; do " \
+			"echo Transferring firmware image part ${i} of ${update_fw_parts}; " \
+			"if itest ${i} -le f; then " \
+				"setenv j 0${i}; " \
+			"else " \
+				"setenv j ${i}; " \
+			"fi; " \
+			"if tftp ${loadaddr} ${update_fw_basedir}${update_fw_filename_prefix}${j}${update_fw_filename_suffix}; then " \
+				"setexpr k ${i} - 1; " \
+				"setexpr offset ${update_fw_fsize_uncompressed} * ${k}; " \
+				"if gzwrite mmc ${mmcdev} ${loadaddr} ${filesize} ${gzwrite_wbuf} ${offset}; then " \
+					"setexpr i ${i} - 1; " \
+				"else " \
+					"setexpr i 0; " \
+					"setexpr error 1; " \
+				"fi; " \
+			"else " \
+				"setexpr i 0; " \
+				"setexpr error 1; " \
+			"fi; " \
+		"done; setenv i; setenv j; setenv k; setenv fsize; setenv filesize; setenv offset; " \
+		"if test ${error} -eq 1; then " \
+			"echo Firmware Update FAILED; " \
+		"else " \
+			"echo Firmware Update OK; " \
+		"fi; setenv error\0" \
 	CONFIG_MFG_ENV_SETTINGS \
 	"image=zImage\0" \
 	"console=ttymxc3\0" \
@@ -85,41 +117,64 @@
 	"initrd_high=0xffffffff\0" \
 	ENV_FDT_FILE \
 	"fdt_addr=0x83000000\0" \
-	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
-	"bootsys=1\0" \
+	"BOOT_ORDER=A B\0" \
+	"BOOT_A_LEFT=3\0" \
+	"BOOT_B_LEFT=3\0" \
 	"mmcdev=0\0" \
 	"mmcpart=1\0" \
 	"mmcroot=/dev/mmcblk1p1\0" \
-	"mmcargs=setenv bootargs console=${console},${baudrate} " \
-		"root=${mmcroot} rootfstype=ext4 rootwait rw bootsys=${bootsys} panic=1\0" \
+	"mmcargs=setenv bootargs ${bootargs} console=${console},${baudrate} panic=1\0" \
+	"common_mmcargs=rootfstype=ext4 rootwait\0" \
 	"loadimage=ext4load mmc ${mmcdev}:${mmcpart} ${loadaddr} /boot/${image}\0" \
 	"loadfdt=ext4load mmc ${mmcdev}:${mmcpart} ${fdt_addr} /boot/${fdt_file}\0" \
 	"mmcboot=echo Booting from mmc ...; " \
-		"setenv mmcpart ${bootsys}; " \
-		"setenv mmcroot /dev/mmcblk1p${mmcpart}; " \
-		"run mmcargs; " \
-		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
-			"if run loadfdt; then " \
-				"bootz ${loadaddr} - ${fdt_addr}; " \
-			"else " \
-				"if test ${boot_fdt} = try; then " \
-					"bootz; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
+		"test -n \"${BOOT_ORDER}\" || setenv BOOT_ORDER \"A B\"; " \
+		"test -n \"${BOOT_A_LEFT}\" || setenv BOOT_A_LEFT 3; " \
+		"test -n \"${BOOT_B_LEFT}\" || setenv BOOT_B_LEFT 3; " \
+		"setenv bootargs; " \
+		"for BOOT_SLOT in \"${BOOT_ORDER}\"; do " \
+			"if test \"x${bootargs}\" != x; then " \
+				"; " \
+			"elif test x${BOOT_SLOT} = xA; then " \
+				"if test ${BOOT_A_LEFT} -gt 0; then " \
+					"setexpr BOOT_A_LEFT ${BOOT_A_LEFT} - 1; " \
+					"echo \"Found valid slot A, ${BOOT_A_LEFT} attempts remaining\"; " \
+					"setenv mmcpart 1; " \
+					"setenv mmcroot /dev/mmcblk1p${mmcpart}; " \
+					"setenv bootargs ${common_mmcargs} root=${mmcroot} rauc.slot=A; " \
+				"fi; " \
+			"elif test \"x${BOOT_SLOT}\" = xB; then " \
+				"if test ${BOOT_B_LEFT} -gt 0; then " \
+					"setexpr BOOT_B_LEFT ${BOOT_B_LEFT} - 1; " \
+					"echo \"Found valid slot B, ${BOOT_B_LEFT} attempts remaining\"; " \
+					"setenv mmcpart 2; " \
+					"setenv mmcroot /dev/mmcblk1p${mmcpart}; " \
+					"setenv bootargs ${common_mmcargs} root=${mmcroot} rauc.slot=B; " \
 				"fi; " \
 			"fi; " \
+		"done; " \
+		"if test -n \"${bootargs}\"; then " \
+			"saveenv; " \
 		"else " \
-			"bootz; " \
-		"fi;\0" \
+			"echo \"No valid slot found, resetting tries to 3\"; " \
+			"setenv BOOT_A_LEFT 3; " \
+			"setenv BOOT_B_LEFT 3; " \
+			"saveenv; " \
+			"reset; " \
+		"fi; " \
+		"run mmcargs; " \
+		"run loadimage; run loadfdt; " \
+		"bootz ${loadaddr} - ${fdt_addr}\0" \
 	"erase_mmc=mmc erase 0 800\0" \
 	"erase_env=mmc erase 800 1000\0" \
 	"erase_env1=mmc erase 800 800\0" \
 	"erase_env2=mmc erase 1000 800\0" \
+	"nfsroot=/\0" \
 	"netargs=setenv bootargs console=${console},${baudrate} " \
 		"root=/dev/nfs " \
-	"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
-		"netboot=echo Booting from net ...; " \
+		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
+	"netboot=echo Booting from net ...; " \
 		"run netargs; " \
 		"if test ${ip_dyn} = yes; then " \
 			"setenv get_cmd dhcp; " \
@@ -127,29 +182,16 @@
 			"setenv get_cmd tftp; " \
 		"fi; " \
 		"${get_cmd} ${image}; " \
-		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
-			"if ${get_cmd} ${fdt_addr} ${fdt_file}; then " \
-				"bootz ${loadaddr} - ${fdt_addr}; " \
-			"else " \
-				"if test ${boot_fdt} = try; then " \
-					"bootz; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi; " \
-			"fi; " \
-		"else " \
-			"bootz; " \
-		"fi;\0" \
+		"${get_cmd} ${fdt_addr} ${fdt_file}; " \
+		"bootz ${loadaddr} - ${fdt_addr}\0"
 
 #define CONFIG_BOOTCOMMAND \
-	"mmc dev ${mmcdev};" \
+	"mmc dev ${mmcdev}; " \
 	"if mmc rescan; then " \
-		"if run loadimage; then " \
-			"run mmcboot; " \
-		"else " \
-			"run netboot; " \
-		"fi; " \
-	"else run netboot; fi" \
+		"run mmcboot; " \
+	"else " \
+		"run netboot; " \
+	"fi"
 
 /* Miscellaneous configurable options */
 #define CONFIG_OF_BOARD_SETUP
