9265fa31ecfe5f41656f1bf0cc8b92392d5a7ffd
Michael Heimpold <michael.heimpold@i2se.com>
2018-08-13 12:07:51 +0200

    tarragon: cleanup FEC initialization
    Signed-off-by: Michael Heimpold <michael.heimpold@i2se.com>

--- board/i2se/tarragon/tarragon.c
+++ board/i2se/tarragon/tarragon.c
@@ -254,21 +254,12 @@ static int setup_fec(int fec_id)
 	struct iomuxc *const iomuxc_regs = (struct iomuxc *)IOMUXC_BASE_ADDR;
 	int ret;
 
-	if (fec_id == 0) {
-		/*
-		 * Use 50M anatop loopback REF_CLK1 for ENET1,
-		 * clear gpr1[13], set gpr1[17].
-		 */
-		clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC1_MASK,
-				IOMUX_GPR1_FEC1_CLOCK_MUX1_SEL_MASK);
-	} else {
-		/*
-		 * Use 50M anatop loopback REF_CLK2 for ENET2,
-		 * clear gpr1[14], set gpr1[18].
-		 */
-		clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC2_MASK,
-				IOMUX_GPR1_FEC2_CLOCK_MUX1_SEL_MASK);
-	}
+	/*
+	 * Use 50M anatop loopback REF_CLK1 for ENET1,
+	 * clear gpr1[13], set gpr1[17].
+	 */
+	clrsetbits_le32(&iomuxc_regs->gpr[1], IOMUX_GPR1_FEC1_MASK,
+	                IOMUX_GPR1_FEC1_CLOCK_MUX1_SEL_MASK);
 
 	ret = enable_fec_anatop_clock(fec_id, ENET_50MHZ);
 	if (ret)
